83fe58807d6c59bf53fc8c6a270c51ba50e16918
HBedwell24 <bedwellhb@gmail.com>
2019-09-12 10:30:34 -0400

    Added an additional tutorial for login/register functionality in ASP.NET

---
+++ RegistrationAndLogin/Controllers/UserController.cs
@@ -0,0 +1,203 @@
+﻿using RegistrationAndLogin.Models;
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace RegistrationAndLogin.Controllers
+{
+    public class UserController : Controller
+    {
+        // Registration Action
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        // Registration POST action
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration([Bind(Exclude = "IsEmailVerfied,ActivationCode")] User user)
+        {
+            bool Status = false;
+            string message = "";
+
+            if (ModelState.IsValid)
+            {
+                #region Check to see if email already exists
+                var isExistent = IsEmailExist(user.EmailID);
+                if (isExistent)
+                {
+                    ModelState.AddModelError("EmailExist", "Email already exists.");
+                    return View(user);
+                }
+                #endregion
+
+                #region Generate Activation Code
+                user.ActivationCode = Guid.NewGuid();
+                #endregion
+
+                #region Password Hashing
+                user.Password = Crypto.Hash(user.Password);
+                user.ConfirmPassword = Crypto.Hash(user.ConfirmPassword);
+                #endregion
+
+                user.IsEmailVerified = false;
+
+                #region Save to database
+                using (MyDatabaseEntities dc = new MyDatabaseEntities())
+                {
+                    dc.Users.Add(user);
+                    dc.SaveChanges();
+
+                    // Send email to user
+                    SendVerificationLinkEmail(user.EmailID, user.ActivationCode.ToString());
+                    message = "Registration successful! Account activation link " +
+                        " has been sent to " + user.EmailID + "!";
+                    Status = true;
+                }
+                #endregion
+            }
+            else
+            {
+                message = "Invalid Request";
+            }
+            ViewBag.Message = message;
+            ViewBag.Status = Status;
+            return View(user);
+        }
+
+        // Verify Account
+        [HttpGet]
+        public ActionResult VerifyAccount(string id)
+        {
+            bool Status = false;
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                dc.Configuration.ValidateOnSaveEnabled = false;
+                var v = dc.Users.Where(a => a.ActivationCode == new Guid(id)).FirstOrDefault();
+                if (v != null)
+                {
+                    v.IsEmailVerified = true;
+                    dc.SaveChanges();
+                    Status = true;
+                }
+                else
+                {
+                    ViewBag.Message = "Invalid Request";
+                }
+            }
+            ViewBag.Status = Status;
+            return View();
+        }
+
+        // Login
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // Login POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserLogin login, string ReturnUrl)
+        {
+            string message = "";
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var v = dc.Users.Where(a => a.EmailID == login.EmailID).FirstOrDefault();
+                if (v != null)
+                {
+                    if (string.Compare(Crypto.Hash(login.Password), v.Password) == 0)
+                    {
+                        int timeout = login.RememberMe ? 525600 : 20;
+                        var ticket = new FormsAuthenticationTicket(login.EmailID, login.RememberMe, timeout);
+                        string encrypted = FormsAuthentication.Encrypt(ticket);
+                        var cookie = new HttpCookie(FormsAuthentication.FormsCookieName, encrypted);
+                        cookie.Expires = DateTime.Now.AddMinutes(timeout);
+                        cookie.HttpOnly = true;
+                        Response.Cookies.Add(cookie);
+
+                        if (Url.IsLocalUrl(ReturnUrl))
+                        {
+                            return Redirect(ReturnUrl);
+                        }
+                        else
+                        {
+                            return RedirectToAction("Index", "Home");
+                        }
+                    }
+                    else
+                    {
+                        message = "Invalid credentials provided.";
+                    }
+                }
+                else
+                {
+                    message = "Invalid credentials provided.";
+                }
+            }
+            ViewBag.Message = message;
+            return View();
+        }
+
+        // Logout
+        [Authorize]
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "User");
+        }
+
+
+        [NonAction]
+        public bool IsEmailExist(string emailID)
+        {
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var email = dc.Users.Where(a => a.EmailID == emailID).FirstOrDefault();
+                return email != null;
+            }
+        }
+
+        [NonAction]
+        public void SendVerificationLinkEmail(string emailID, string activationCode)
+        {
+            var verifyUrl = "/User/VerifyAccount/" + activationCode;
+            var link = Request.Url.AbsoluteUri.Replace(Request.Url.PathAndQuery, verifyUrl);
+
+            var fromEmail = new MailAddress("bedwellhb@gmail.com");
+            var toEmail = new MailAddress(emailID);
+            var fromEmailPassword = "";
+            string subject = "Your account was successfully created!";
+
+            string body = "<br/><br/>We are excited to tell you that your account was" +
+                " successfully created. Please click on the link below to verify your account" +
+                " <br/><br/><a href='" + link + "'>" + link + "</a> ";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromEmail.Address, fromEmailPassword)
+            };
+
+            using (var message = new MailMessage(fromEmail, toEmail)
+            {
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            })
+            smtp.Send(message);
+        }
+    }
+}
